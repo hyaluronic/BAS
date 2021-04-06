ed9e082e8c4b4b2109d26e8b35bd354b33a42562
hyaluronic <60736298+hyaluronic@users.noreply.github.com>
2021-04-06 15:12:16 +0300

    Change: refactored main method

--- src/com/company/service/ValidationService.java
+++ src/com/company/service/ValidationService.java
@@ -1,28 +1,81 @@
 package com.company.service;
 
+import com.company.domain.constants.ResultsEnum;
 import com.company.domain.entity.Person;
 
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.Integer.parseInt;
+
 public class ValidationService {
 
-    public static boolean isIdValid(int id, List<Person> people) {
-        return people.stream().noneMatch(p -> p.getId() == id);
+    public static void idValidation(String idString, List<Person> people) throws Exception {
+        if (idString.isBlank()) {
+            throw new Exception(ResultsEnum.Results.INVALID_NO_ID.toString());
+        } else if (checkForAdditionalSpaceCharacters(idString)) {
+            throw new Exception(ResultsEnum.Results.INVALID_SPACE.toString());
+        } else {
+            try {
+                int id = parseInt(idString);
+                if (isIdDuplicate(id, people)) {
+                    throw new Exception(ResultsEnum.Results.INVALID_ID_DUPLICATE.toString());
+                }
+            } catch (IllegalArgumentException e) {
+                throw new Exception(ResultsEnum.Results.INVALID_ID.toString());
+            }
+        }
     }
 
-    public static boolean isNameValid(String name) {
+    public static void nameValidation(String name, List<Person> people) throws Exception {
+        if (name.isBlank()) {
+            throw new Exception(ResultsEnum.Results.INVALID_NO_NAME.toString());
+        } else if (checkForAdditionalSpaceCharacters(name)) {
+            throw new Exception(ResultsEnum.Results.INVALID_SPACE.toString());
+        } else if (!isNameValid(name)) {
+            throw new Exception(ResultsEnum.Results.INVALID_NAME.toString());
+        } else if (isNameDuplicate(name, people)) {
+            throw new Exception(ResultsEnum.Results.INVALID_NAME_DUPLICATE.toString());
+        }
+    }
+
+    public static void coefficientValidation(String coefficient) throws Exception {
+        if (coefficient.isBlank()) {
+            throw new Exception(ResultsEnum.Results.INVALID_NO_COEFFICIENT.toString());
+        } else if (checkForSpaceCharacter(coefficient)) {
+            throw new Exception(ResultsEnum.Results.INVALID_SPACE.toString());
+        } else if (!isCoefficientValid(coefficient)) {
+            throw new Exception(ResultsEnum.Results.INVALID_COEFFICIENT.toString());
+        }
+    }
+
+    private static boolean checkForSpaceCharacter(String string) {
+        Pattern pattern = Pattern.compile("\\s");
+        Matcher matcher = pattern.matcher(string);
+        return matcher.find();
+    }
+
+    private static boolean isIdDuplicate(int id, List<Person> people) {
+        return people.stream().anyMatch(p -> p.getId() == id);
+    }
+
+    private static boolean checkForAdditionalSpaceCharacters(String string) {
+        String[] nameSplitBySpace = string.split(" ", 3);
+        return nameSplitBySpace.length > 2;
+    }
+
+    private static boolean isNameValid(String name) {
         Pattern pattern = Pattern.compile("^[\\w]+\\s[\\w]+$");
         Matcher matcher = pattern.matcher(name);
         return matcher.find();
     }
 
-    public static boolean isNameDuplicate(String name, List<Person> people) {
+    private static boolean isNameDuplicate(String name, List<Person> people) {
         return people.stream().anyMatch(p -> p.getName().equals(name));
     }
 
-    public static boolean isCoefficientValid(String coefficient) {
+    private static boolean isCoefficientValid(String coefficient) {
         Pattern pattern = Pattern.compile("^[\\d]+(\\.[\\d]{1,4})?$");
         Matcher matcher = pattern.matcher(coefficient);
         if (matcher.find()) {
@@ -30,16 +83,5 @@ public class ValidationService {
             return value >= 1 && value <= 2.5;
         }
         return false;
-    }
-
-    public static boolean checkForSpaceCharacter(String string) {
-        Pattern pattern = Pattern.compile("\\s");
-        Matcher matcher = pattern.matcher(string);
-        return matcher.find();
-    }
-
-    public static boolean checkForAdditionalSpaceCharacters(String string) {
-        String[] nameSplitBySpace = string.split(" ", 3);
-        return nameSplitBySpace.length>2;
     }
 }
